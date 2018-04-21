d7d1829b1c3720b337409143ff13ad493e164322
karenhoyos <karenuca@hotmail.com>
2018-04-21 17:12:18 +0200

    Added validation on the length description recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
 validates :name, presence: true
-validates :description, presence: true
+validates :description, presence: true, length: { minimum: 5, maximum: 500 }
 end
