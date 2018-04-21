a260bd432671a45c1ffd4f71b7f1db7a07a4e119
karenhoyos <karenuca@hotmail.com>
2018-04-21 16:42:00 +0200

    Schema changed after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180421143615) do
+ActiveRecord::Schema.define(version: 20180421144003) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "chefname"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
