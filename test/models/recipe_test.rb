e2530fddbf9bb140ce603cfb0fa16b3f0f3be96f
karenhoyos <karenuca@hotmail.com>
2018-04-21 17:11:39 +0200

    Run test to validate length of description recipe

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -6,6 +6,8 @@ class RecipeTest < ActiveSupport::TestCase
     @recipe = Recipe.new(name: "paella", description: "great dish for all the family")
   end
 
+  # Test to validate the name of the recipe
+  # The same can be done with the description
   test "recipe should be valid" do
     assert @recipe.valid?
   end
@@ -15,4 +17,15 @@ class RecipeTest < ActiveSupport::TestCase
     assert_not @recipe.valid?
   end
 
+
+  # Test to validate the length of the description of the recipe
+  test "description shouldn't contain less than 5 characters" do
+    @recipe.description = "a" * 3
+    assert_not @recipe.valid?
+  end
+
+  test "description shouldn't contain more than 500 characters" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
 end
