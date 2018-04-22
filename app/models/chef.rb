1cb99e639fbbc7c3abb748fa5b17c1a3dcd94d9b
karenhoyos <karenuca@hotmail.com>
2018-04-22 09:22:24 +0200

    Validations added in the Chef Model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,8 @@
 class Chef < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :chefname, presence: true, length: { maximum: 30 }
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
