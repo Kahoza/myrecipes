40474fb73213fda11c2948001335b0a12a4e4e58
karenhoyos <karenuca@hotmail.com>
2018-04-22 09:39:29 +0200

    Added foreign key for Chef Model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,4 +5,5 @@ class Chef < ApplicationRecord
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_many :recipes
 end
