9842cc2dbaffd9829aa56efee91207592a429a23
karenhoyos <karenuca@hotmail.com>
2018-04-22 19:02:47 +0200

    Added show page, route and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "pages#home"
+  root 'pages#home'
   get 'pages/home', to: 'pages#home'
 
+  get 'recipes/:id', to: 'recipes#show', as: 'recipe'
   get '/recipes', to: 'recipes#index'
+
 end
