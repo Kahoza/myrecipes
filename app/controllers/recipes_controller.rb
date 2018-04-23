8c17de710f9cdc70958da569d40bdb1fe9b23945
karenhoyos <karenuca@hotmail.com>
2018-04-23 11:31:53 +0200

    Added new method in recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,4 +12,22 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    # To be changed once chefs are added to the project
+    # For now, the recipes will be assigned to the first chef created
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:success] = "Recipe was successfully created!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
+
 end
