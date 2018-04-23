4771ac97ea82a107e3bd39c254401cc35ff8c1b4
karenhoyos <karenuca@hotmail.com>
2018-04-23 14:38:23 +0200

    Test for delete action

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "karen", email: "karen@example.com")
+    @recipe = Recipe.create(name: "Pancakes", description: "Super easy recipe", chef: @chef)
+  end
+
+  test "recipe successfully deleted" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_difference 'Recipe.count', -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to recipes_path
+    assert_not flash.empty?
+  end
+
+end
