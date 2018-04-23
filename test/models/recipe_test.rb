8cc8907150654c794d30a1f2eb1c75d9ea01b227
karenhoyos <karenuca@hotmail.com>
2018-04-23 16:47:45 +0200

    Model test to add the password in Chefs

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @chef = Chef.create!(chefname: "karen", email: "karen@example.com")
+    @chef = Chef.create!(chefname: "karen", email: "karen@example.com",
+                         password: "password", password_confirmation: "password")
     @recipe = @chef.recipes.build(name: "paella", description: "great dish for all the family")
   end
 
