d194048ff410bde50f0ba3379a37429260ce159d
karenhoyos <karenuca@hotmail.com>
2018-04-23 14:38:50 +0200

    Delete method added in recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -39,6 +39,12 @@ class RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe successfully deleted"
+    redirect_to recipes_path
+  end
+
   private
 
   def recipe_params
