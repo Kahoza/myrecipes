0a96eaff6247b1053510f01dbcef63d17218e9b6
karenhoyos <karenuca@hotmail.com>
2018-04-23 18:44:04 +0200

    Method added to chefs controller

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -16,7 +16,20 @@ class ChefsController < ApplicationController
 
   def show
     @chef = Chef.find(params[:id])
+  end
 
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your details were successfully updated"
+      redirect_to @chef
+    else
+      render 'edit'
+    end
   end
 
   private
