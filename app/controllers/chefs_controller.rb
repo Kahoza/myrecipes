f6fba9fdfb4a86b97a4411bb520356c58cfa3b43
karenhoyos <karenuca@hotmail.com>
2018-04-24 12:55:56 +0200

    Fixed typo, used {} instead of []

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,6 @@
 class ChefsController < ApplicationController
 
-  before_action :set_chef, only: {:show, :edit, :update, :delete}
+  before_action :set_chef, only: [:show, :edit, :update, :delete]
 
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 6)
