f39ff47bcc20d4693eb85fd2a8d547ea813ba250
karenhoyos <karenuca@hotmail.com>
2018-04-24 18:48:20 +0200

    Added Admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423142957) do
+ActiveRecord::Schema.define(version: 20180424151008) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180423142957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "recipes", force: :cascade do |t|
