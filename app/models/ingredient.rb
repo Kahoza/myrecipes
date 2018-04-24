f40efb6cc7f3198dd6725bd6a48f46e5d94dc43b
karenhoyos <karenuca@hotmail.com>
2018-04-24 20:19:15 +0200

    Added ingredient, join table and validations

---
+++ app/models/ingredient.rb
@@ -0,0 +1,7 @@
+class Ingredient < ApplicationRecord
+  validates :name, presence: true, length: {minimum: 3, maximum: 30}
+  validates_uniqueness_of :name
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424180321) do
+ActiveRecord::Schema.define(version: 20180424180848) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20180424180321) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
