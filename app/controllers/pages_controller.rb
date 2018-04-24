c894bf0682594b96319131d91daa33f1df021b2b
karenhoyos <karenuca@hotmail.com>
2018-04-24 19:37:27 +0200

    Added about, help and testimonials section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
   def home
     redirect_to recipes_path if logged_in?
   end
+
+  def about
+  end
+
+  def help
+  end
+
+  def testimonials
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'pages/home', to: 'pages#home'
+  get 'pages/about', to: 'pages#about'
+  get 'pages/help', to: 'pages#help'
+  get 'pages/testimonials', to: 'pages#testimonials'
 
   resources :recipes
 
