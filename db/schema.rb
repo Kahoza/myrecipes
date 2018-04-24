249e43d119c7f19e133459cf7e7de7b6a2808e7e
karenhoyos <karenuca@hotmail.com>
2018-04-24 20:04:11 +0200

    Added ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424151008) do
+ActiveRecord::Schema.define(version: 20180424180321) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20180424151008) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "recipes", force: :cascade do |t|
