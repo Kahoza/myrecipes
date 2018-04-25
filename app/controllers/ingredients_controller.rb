feb6a247d5024bd0af7d16488a97c7dc75ce0242
karenhoyos <karenuca@hotmail.com>
2018-04-25 09:10:41 +0200

    Added ingredients show page, and link ingredients with recipes containing that ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,16 +1,27 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:edit, :update, :show]
 
   def index
     @ingredients = Ingredient.paginate(page: params[:page], per_page: 6)
   end
 
   def show
+    @ingredient_recipes = @ingredient.recipes.paginate(page: params[:page], per_page: 6)
   end
 
   def new
   end
 
   def create
+  end
+
+  def update
+  end
+
+  private
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
   end
 
 end
