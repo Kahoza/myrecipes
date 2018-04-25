15e93a9bd67e314dd3bdb214e435633e5bfb0b62
karenhoyos <karenuca@hotmail.com>
2018-04-25 18:30:29 +0200

    Added comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  before_action :require_user
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = @recipe.comments.build(comment_params)
+    @comment.chef = current_chef
+    if @comment.save
+      flash[:success] = "Your comment was successfully created"
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:danger] = "Your comment wasn't saved"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:description)
+  end
+end
