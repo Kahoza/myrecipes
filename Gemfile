b7931dd8b4f31d3bd5f315b84a68a222a95e5069
karenhoyos <karenuca@hotmail.com>
2018-04-26 15:33:42 +0200

    Preparing to deplay with Actio Cable

--- Gemfile
+++ Gemfile
@@ -64,6 +64,7 @@ end
 
 group :production do
   gem 'pg'
+  gem 'redis'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://"myrecipescommunity.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://myrecipescommunity.herokuapp.com', 'http://myrecipescommunity.herokuapp.com']
+
 end
